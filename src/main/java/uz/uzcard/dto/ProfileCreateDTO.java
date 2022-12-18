51e737d5c40e724653531b0e72efec9cee564fc7
otabek <otabekhoshimxon@gmail.com>
2022-12-18 23:54:14 +0500

    added profile update

--- src/main/java/uz/uzcard/dto/ProfileCreateDTO.java
+++ src/main/java/uz/uzcard/dto/ProfileCreateDTO.java
@@ -3,6 +3,7 @@ package uz.uzcard.dto;
 import lombok.*;
 
 import javax.validation.constraints.Email;
+import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
@@ -10,11 +11,15 @@ import javax.validation.constraints.Email;
 @NoArgsConstructor
 @AllArgsConstructor
 public class ProfileCreateDTO {
+    @NotNull
     private String name;
+    @NotNull
     private String surname;
     @Email
     private String email;
+    @NotNull
     private String username;
+    @NotNull
     private String password;
 
 
